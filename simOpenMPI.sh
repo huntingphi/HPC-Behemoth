b4fbb29f3c79a7a18cc41ecfec80ec1d7ef2bf6e
Jethro <jethro@pop-os.localdomain>
2019-05-31 03:04:48 +0200

    edited sim files

--- simOpenMPI.sh
+++ simOpenMPI.sh
@@ -1,5 +1,5 @@
-make -s OpenMPI ARGS="2000000 1" >> simOpenMPI.csv
-make -s OpenMPI ARGS="4000000 1" >> simOpenMPI.csv
-make -s OpenMPI ARGS="6000000 1" >> simOpenMPI.csv
-make -s OpenMPI ARGS="8000000 1" >> simOpenMPI.csv
-make -s OpenMPI ARGS="10000000 1" >> simOpenMPI.csv
+make -s OpenMPI ARGS="2000000 1" >> simOpenMPI2$1.csv
+make -s OpenMPI ARGS="4000000 1" >> simOpenMPI4$1.csv
+make -s OpenMPI ARGS="6000000 1" >> simOpenMPI6$1.csv
+make -s OpenMPI ARGS="8000000 1" >> simOpenMPI8$1.csv
+make -s OpenMPI ARGS="10000000 1" >> simOpenMPI10$1.csv
