2f21ea588f1d19abb279821a46cd41cef81893b0
Jethro <jethro@pop-os.localdomain>
2019-05-31 02:04:37 +0200

    Fixed sim scripts

--- simOpenMPI.sh
+++ simOpenMPI.sh
@@ -1,5 +1,5 @@
-make OpenMPI-s ARGS="2000000 1" >> simOpenMPI.csv
-make OpenMPI-s ARGS="4000000 1" >> simOpenMPI.csv
-make OpenMPI-s ARGS="6000000 1" >> simOpenMPI.csv
-make OpenMPI-s ARGS="8000000 1" >> simOpenMPI.csv
-make OpenMPI-s ARGS="10000000 1" >> simOpenMPI.csv
+make -s OpenMPI ARGS="2000000 1" >> simOpenMPI.csv
+make -s OpenMPI ARGS="4000000 1" >> simOpenMPI.csv
+make -s OpenMPI ARGS="6000000 1" >> simOpenMPI.csv
+make -s OpenMPI ARGS="8000000 1" >> simOpenMPI.csv
+make -s OpenMPI ARGS="10000000 1" >> simOpenMPI.csv
